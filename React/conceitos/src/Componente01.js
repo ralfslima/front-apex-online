8ba3035775c1587d7800f935524332778e8d84ab
ralflima <ralf.lima@gmail.com>
2024-08-14 21:16:59 -0300

    React - Aula 01

---
+++ React/conceitos/src/Componente01.js
@@ -0,0 +1,18 @@
+// Importar o React
+import React from 'react';
+
+// Função (Componente)
+function Componente01(){
+
+    // Retorno
+    return(
+        <div>
+            <h1>Hello World!</h1> 
+            <h1>Aprendendo React</h1>
+        </div>
+    );
+
+}
+
+// Exportar o componente
+export default Componente01;
---
+++ React/conceitos/src/index.js
@@ -0,0 +1,18 @@
+// Importarções
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
+// Importar componentes
+import Componente01 from './Componente01';
+
+// 1º Render - SEM JSX
+//const elemento = React.createElement('h1', {}, 'Hello World!');
+//ReactDOM.render(elemento, document.getElementById('root'));
+
+// 2º Render - COM JSX (Render utilizado até a versão 17)
+//ReactDOM.render(<h1>Hello React</h1>, document.getElementById('root'));
+
+// 3º Render - COM JSX (Render utilizado a partir da versão 18)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Componente01/>);
